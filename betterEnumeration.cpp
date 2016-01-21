d29d0b2f9bc6d1733fd9165f700095e1f2aef5f6
nockc <nockc@oregonstate.edu>
2016-01-21 02:05:59 -0800

    Create betterEnumeration.cpp

---
+++ betterEnumeration.cpp
@@ -0,0 +1,78 @@
+/**************************************************************
+ * *  Filename: betterEnumeration.cpp
+ * *  Coded by: Dustin Dinh, Benjamin Fisher, Christina Nock
+ * *  Purpose:  To run a better brute force enumeration algorithm for the 
+ * *			max subarray problem.
+ * *
+ * ***************************************************************/
+
+#include "betterEnumeration.h"
+#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <stdlib.h>
+
+/**************************************************************
+ * *  Function name: bruteForceEnumeration
+ * *  Parameters: integer vector as input to run algorithm on
+ * *  Return value: int representing sum of	the max subarray
+ * *  Description: Function will find the sub array with the max sum 
+ * *  in the array arg
+ * ***************************************************************/
+int betterBruteMaxSum( std::vector <int> v )
+{
+	int maxSum = 0;				//maximum sum subarray calculated so far
+	int currentSum = 0;			//current sum of current subarray
+	/* Start at index 0 and iterate through until the end */
+	for ( int startIndex = 0; startIndex < v.size(); startIndex++ )
+	{
+		/* Reset sum to 0 before calculating next summation */
+		currentSum = 0;
+		/* Each iteration will increase the sub array by one */
+		for ( int i = startIndex; i < v.size(); i++ )
+		{
+			/* Calculate next summation between new startIndex and i values */
+			currentSum += v[i];
+			/* If current summation is greater than previous max update max and indices */
+			if ( currentSum > maxSum )
+				maxSum = currentSum;
+		}
+	}
+	return maxSum;
+}
+
+/**************************************************************
+ * *  Function name: bruteForceEnumeration
+ * *  Parameters: integer vector as input to run algorithm on
+ * *  Return value: int representing sum of	the max subarray
+ * *  Description: Function will find the sub array with the max sum 
+ * *  in the array arg
+ * ***************************************************************/
+int betterBruteMaxSum( std::vector <int> v, int &lowInd, int &highInd )
+{
+	int maxSum = 0;				//maximum sum subarray calculated so far
+	int currentSum = 0;			//current sum of current subarray
+	int lowIndex = 0;			//set the lower index to start summing from
+	int highIndex = 0;			//set the higher index to end summing from
+	/* Start at index 0 and iterate through until the end */
+	for ( int startIndex = 0; startIndex < v.size(); startIndex++ )
+	{
+		/* Reset sum to 0 before calculating next summation */
+		currentSum = 0;
+		/* Each iteration will increase the sub array by one */
+		for ( int i = startIndex; i < v.size(); i++ )
+		{
+			/* Calculate next summation between new startIndex and i values */
+			currentSum += v[i];
+			/* If current summation is greater than previous max update max and indices */
+			if ( currentSum > maxSum )
+			{
+				maxSum = currentSum;
+				lowIndex = startIndex;
+				highIndex = i;
+			}
+		}
+	}
+	return maxSum;
+}
