3011aa4eac4238e113ed14994484b3df51de30d8
nockc <nockc@oregonstate.edu>
2016-01-21 02:07:48 -0800

    Create enumeration.h

---
+++ enumeration.h
@@ -0,0 +1,4 @@
+#include <vector>			//we are using vector as input
+
+int bruteForceEnumeration( std::vector <int> v );
+int bruteForceEnumeration(std::vector <int> v, int &lowIndex, int &highIndex);
